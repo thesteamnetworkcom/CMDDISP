e8fd8ca9a9fa46977a50f6cfa4f739d3f241a1ef
Scott Lewis <scottiemlewis1991@gmail.com>
2019-05-28 07:17:59 -0600

    save?
    save?

--- js/footer.js
+++ js/footer.js
@@ -7,6 +7,19 @@ class Footer extends React.Component{
         return(
             <React.Fragment>
                 <div className='footer'>
+                    <div className='footer-wrapper'>
+                        <span className='copyright'>Copyright 2019</span>
+                    </div>
+                    <div className='footer-wrapper'>
+                        <span className='social'>
+                            <i class="fab fa-twitter"></i>
+                            <i class="fab fa-facebook"></i>
+                            <i class="fab fa-github"></i>
+                            <i class="fab fa-discord"></i>
+                            <i class="fab fa-twitch"></i>
+                            <i class="fab fa-wizards-of-the-coast"></i>
+                        </span>
+                    </div>
                 </div>
             </React.Fragment>
         )
